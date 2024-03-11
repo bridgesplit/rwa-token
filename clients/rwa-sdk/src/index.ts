6b3c240400575736379ab0087d1a5af1cc612dd3
Luke Truitt <luke@truittluke.com>
2024-03-11 13:42:55 -0400

    updated package naming

--- clients/rwa-sdk/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './programs';
-export * from './utils';
-export * from './asset_controller';
-export * from './data_registry';
-export * from './identity_registry';
-export * from './policy_engine';
