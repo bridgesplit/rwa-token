73d6fdf7859fa25cbf1eecfbe89e94d2e13bc89c
Bhargava Sai Macha <mbsai29@gmail.com>
2024-03-22 16:55:46 -0400

    add idl build

---
+++ clients/rwa-token-sdk/tests/policies/transaction_amount.test.ts
@@ -0,0 +1,6 @@
+
+import {describe, expect} from 'vitest';
+
+describe('test policy setup', () => {
+	expect(true).toBeTruthy();
+});
--- clients/rwa-token-sdk/tests/tracker.test.ts
+++ clients/rwa-token-sdk/tests/tracker.test.ts
@@ -1,1 +1,7 @@
 // Test file is going to test the tracker account and if its storing the right amount of data according to the policies attached
+
+import {describe, expect} from 'vitest';
+
+describe('test policy setup', () => {
+	expect(true).toBeTruthy();
+});
