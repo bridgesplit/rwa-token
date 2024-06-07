5f8730df7061c494552e5e390b71fd2cfe3e649c
Chris Hagedorn <1844858+ChrisHagedorn@users.noreply.github.com>
2024-06-07 09:10:26 -0400

    Remove anchor wallet copy

--- clients/rwa-token-sdk/src/classes/Client.ts
+++ clients/rwa-token-sdk/src/classes/Client.ts
@@ -3,51 +3,37 @@ import { AssetController } from "./AssetController";
 import { IdentityRegistry } from "./IdentityRegistry";
 import { PolicyEngine } from "./PolicyEngine";
 import { DataRegistry } from "./DataRegistry";
-import { AnchorProvider } from "@coral-xyz/anchor";
-import { Transaction, VersionedTransaction, PublicKey } from "@solana/web3.js";
-
-/**
- * Copy of Anchor Wallet due to version typing mismatch.
- */
-export interface Wallet {
-  signTransaction<T extends Transaction | VersionedTransaction>(
-    tx: T
-  ): Promise<T>;
-  signAllTransactions<T extends Transaction | VersionedTransaction>(
-    txs: T[]
-  ): Promise<T[]>;
-  publicKey: PublicKey;
-}
+import { AnchorProvider, Wallet } from "@coral-xyz/anchor";
 
 /**
  * Represents a client for interacting with Real World Assets (RWA).
  */
 export class RwaClient {
-	/**s
+  /**s
    * Configuration for the RWA client.
    */
-	config: Config;
-	provider: AnchorProvider;
-	assetController: AssetController;
-	dataRegistry: DataRegistry;
-	identityRegistry: IdentityRegistry;
-	policyEngine: PolicyEngine;
+  config: Config;
+  provider: AnchorProvider;
+  assetController: AssetController;
+  dataRegistry: DataRegistry;
+  identityRegistry: IdentityRegistry;
+  policyEngine: PolicyEngine;
 
-	/**
+  /**
    * Constructs a new instance of the RwaClient.
    * @param rwaConfig The configuration for the RWA client.
    * @param wallet Anchor wallet used for provider
    */
-	constructor(config: Config, wallet: Wallet) {
-		this.config = config;
-		this.provider = new AnchorProvider(
-			config.connection,
-			wallet,
-			config.confirmationOptions
-		);
-		this.assetController = new AssetController(this);
-		this.dataRegistry = new DataRegistry(this);
-		this.identityRegistry = new IdentityRegistry(this);
-		this.policyEngine = new PolicyEngine(this);
-	}
+  constructor(config: Config, wallet: Wallet) {
+    this.config = config;
+    this.provider = new AnchorProvider(
+      config.connection,
+      wallet,
+      config.confirmationOptions
+    );
+    this.assetController = new AssetController(this);
+    this.dataRegistry = new DataRegistry(this);
+    this.identityRegistry = new IdentityRegistry(this);
+    this.policyEngine = new PolicyEngine(this);
+  }
 }
