99d487f641cc554e57dc652c5e08483f05e144c5
Bhargava Sai Macha <mbsai29@gmail.com>
2024-06-13 13:51:37 -0400

    lint

--- clients/rwa-token-sdk/src/classes/Client.ts
+++ clients/rwa-token-sdk/src/classes/Client.ts
@@ -9,31 +9,31 @@ import { AnchorProvider, Wallet } from "@coral-xyz/anchor";
  * Represents a client for interacting with Real World Assets (RWA).
  */
 export class RwaClient {
-  /**s
+	/**s
    * Configuration for the RWA client.
    */
-  config: Config;
-  provider: AnchorProvider;
-  assetController: AssetController;
-  dataRegistry: DataRegistry;
-  identityRegistry: IdentityRegistry;
-  policyEngine: PolicyEngine;
+	config: Config;
+	provider: AnchorProvider;
+	assetController: AssetController;
+	dataRegistry: DataRegistry;
+	identityRegistry: IdentityRegistry;
+	policyEngine: PolicyEngine;
 
-  /**
+	/**
    * Constructs a new instance of the RwaClient.
    * @param rwaConfig The configuration for the RWA client.
    * @param wallet Anchor wallet used for provider
    */
-  constructor(config: Config, wallet: Wallet) {
-    this.config = config;
-    this.provider = new AnchorProvider(
-      config.connection,
-      wallet,
-      config.confirmationOptions
-    );
-    this.assetController = new AssetController(this);
-    this.dataRegistry = new DataRegistry(this);
-    this.identityRegistry = new IdentityRegistry(this);
-    this.policyEngine = new PolicyEngine(this);
-  }
+	constructor(config: Config, wallet: Wallet) {
+		this.config = config;
+		this.provider = new AnchorProvider(
+			config.connection,
+			wallet,
+			config.confirmationOptions
+		);
+		this.assetController = new AssetController(this);
+		this.dataRegistry = new DataRegistry(this);
+		this.identityRegistry = new IdentityRegistry(this);
+		this.policyEngine = new PolicyEngine(this);
+	}
 }
